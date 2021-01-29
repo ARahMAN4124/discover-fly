43f972fe55ec94c7ac1fe25b122b38a6e294548c
Abdur Rahman <a.rahman4124@gmail.com>
2021-01-30 04:15:32 +0600

    edit

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document
   .addEventListener("click", function () {
     ticketClass("economy-class", false);
   });
-//First class common function
+//First class and economy common function
 function ticketClass(Class, isIncrease) {
   let ticketInput = document.getElementById(Class);
   let ticketCount = parseInt(ticketInput.value);
@@ -40,7 +40,6 @@ function ticketClass(Class, isIncrease) {
   ticketInput.value = ticketNewCount;
   total();
 }
-
 //Total price
 function total() {
   let firstInput = document.getElementById("first-class");
@@ -49,8 +48,34 @@ function total() {
   let economyCount = parseInt(economyInput.value);
   let subTotal = firstCount * 150 + economyCount * 100;
   document.getElementById("sub-total").innerText = subTotal;
-  let tex = subTotal * 0.1;
-  document.getElementById("tex").innerText = "$" + tex;
-  let total = tex + subTotal;
-  document.getElementById("total").innerText = "$" + total;
+  let vat = subTotal * 0.1;
+  document.getElementById("tax").innerText = vat;
+  let total = vat + subTotal;
+  document.getElementById("total").innerText = total;
+  inputCall();
 }
+//buy now button function
+document.getElementById("booking").addEventListener("click", function () {
+  //step 01
+  //input call to show the amount of first class ticket
+  let firstInputTicket = document.getElementById("first-class");
+  let firstCount = parseInt(firstInputTicket.value);
+  //step 02
+  //input  call to show the amount of economy ticket
+  let economyInputTicket = document.getElementById("economy-class");
+  let economyCount = parseInt(economyInputTicket.value);
+  //step 03
+  //if click buy now button before buy any ticket
+  if (firstCount < 1 && economyCount < 1) {
+    alert("Sorry sir,You didn't buy any ticket");
+  } else {
+    document.getElementById("first-class-ticket").innerText = firstCount;
+    document.getElementById("economy-ticket").innerText = economyCount;
+
+    let form = document.getElementById("booking-form");
+    form.style.display = "none";
+    let after = document.getElementById("after-buy");
+    after.style.display = "block";
+    document.getElementById("main-content").style.marginTop = "5rem";
+  }
+});
